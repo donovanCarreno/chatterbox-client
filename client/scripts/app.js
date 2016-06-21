7a7f30b2f4e389905e062ce6a6e2d250ba1e3a98
donovanCarreno <donovan.carreno@gmail.com>
2016-06-20 19:56:43 -0700

    implement chatterbox

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,31 +1,116 @@
-
 var app = { 
 	
-	var message = {
-  		username: 'shawndrost',
-  		text: 'trololo',
-  		roomname: '4chan'
-	};
+	storedData: {
+	
+	},
 
 	send: function(message) {
 			$.ajax({
-			  // This is the url you should use to communicate with the parse API server.
-			  url: 'https://api.parse.com/1/classes/messages',
-			  type: 'POST',
-			  data: JSON.stringify(message),
-			  contentType: 'application/json',
-			  success: function (data) {
-			    console.log('chatterbox: Message sent');
-			  },
-			  error: function (data) {
-			    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-			    console.error('chatterbox: Failed to send message', data);
-			  }
+				// This is the url you should use to communicate with the parse API server.
+				url: 'https://api.parse.com/1/classes/messages',
+				type: 'POST',
+				data: JSON.stringify(message),
+				contentType: 'application/json',
+				success: function (data) {
+					console.log('chatterbox: Message sent');
+				},
+				error: function (data) {
+		  		// See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+	      	console.error('chatterbox: Failed to send message', data);
+				}
 			});
-		}
+	},
+
+	fetch: function(message) {
+		$.ajax({
+
+			url: 'https://api.parse.com/1/classes/messages',
+			type: 'GET',
+			data: JSON.stringify(message),
+			contentType: 'application/json',
+			success: function(data) {
+				captureData(data);
+				console.log('chatterbox: Messages recieved');
+			},
+			error: function (data) {
+				console.error('chatterbox: Failed to retrieve message', data);
+			}
+
+		});
+	}
+};
 
 
-}
+// another function that retrieves & stores all data
+
+var displayMessages = function(room) {
+	room = room || 'undefined';
+
+	$('#chats').html('');
+
+	for (var i = 0; i < 20; i++) {
+		var $user = $("<div class ='" + app.storedData[room][i].userName + "'></div");
+		var text = app.storedData[room][i].userName + ": " + app.storedData[room][i].text;
+
+		$user.text(text);
+		$user.appendTo('#chats');	
+	}
+};
+
+var captureData = function(data) {
+
+	for (var i = 0; i < data.results.length; i++) {
+		
+		if (data.results[i].roomname !== undefined && data.results[i].username !== undefined && data.results[i].text !== undefined) {
+		
+			var userData = {userName: data.results[i].username,
+										text: data.results[i].text};
+			
+			var roomName = data.results[i].roomname;
+
+			if (app.storedData[roomName] === undefined) {
+				app.storedData[roomName] = [userData];
+			} else {
+				app.storedData[roomName].push(userData);
+			}
+		} 
+	}
+	
+	createRoomsList();
+	displayMessages();
+	
+};
+
+var createRoomsList = function() {
+	for (var key in app.storedData) {
+		$('.rooms').append( new Option(key, key));
+	}
+
+
+$('.rooms').on('change', function() {
+	displayMessages(this.value);
+}); 
+
+
+};
+
+
+$('.sendMessage').on('submit', function() { 
+	console.log("hi");
+
+});
+
+window.onload = app.fetch;
+
+
+
+
+
+
+
+
+
+
 
 
 
